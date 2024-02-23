5262941b127565b9f3f0bed47743bc0082c24e01
Coolzzzer <andrei.zhuk936@gmail.com>
2024-02-23 21:53:24 +0300

    second commit

--- main.js
+++ main.js
@@ -5,17 +5,118 @@ class classWords {
     this.translate = translate
   }
 }
+
+
 const words1 = [
-  new classWords(1,"apple", "яблоко"),
-  new classWords(1,"banana", "банан") 
+  new classWords("Easy", "air", "воздух"), 
+  new classWords("Easy", "animal", "животное"), 
+  new classWords("Easy", "answer", "ответ"), 
+  new classWords("Easy", "area", "область"), 
+  new classWords("Easy", "bird", "птица"), 
+  new classWords("Easy", "body", "тело"), 
+  new classWords("Easy", "book", "книга"), 
+  new classWords("Easy", "bottom", "низ"), 
+  new classWords("Easy", "boy", "мальчик"), 
+  new classWords("Easy", "brother", "брат"), 
+  new classWords("Easy", "car", "машина"), 
+  new classWords("Easy", "child", "ребенок"), 
+  new classWords("Easy", "children", "дети"), 
+  new classWords("Easy", "city", "город"), 
+  new classWords("Easy", "class", "класс"), 
+  new classWords("Easy", "color", "цвет"), 
+  new classWords("Easy", "country", "страна"), 
+  new classWords("Easy", "day", "день"), 
+  new classWords("Easy", "dog", "собака"), 
+  new classWords("Easy", "door", "дверь"), 
+  new classWords("Easy", "east", "восток"), 
+  new classWords("Easy", "example", "пример"), 
+  new classWords("Easy", "eye", "глаз"), 
+  new classWords("Easy", "face", "лицо"), 
+  new classWords("Easy", "family", "семья"), 
+  new classWords("Easy", "farm", "ферма"), 
+  new classWords("Easy", "father", "отец"), 
+  new classWords("Easy", "feet", "ноги"), 
+  new classWords("Easy", "fire", "Огонь"), 
+  new classWords("Easy", "fish", "рыба"), 
+  new classWords("Easy", "food", "еда"), 
+  new classWords("Easy","foot", "нога"), 
+  new classWords("Easy", "friend", "друг"), 
+  new classWords("Easy", "girl", "девочка"), 
+  new classWords("Easy", "hand", "рука"), 
+  new classWords("Easy", "head", "голова"), 
+  new classWords("Easy", "home", "дом"), 
+  new classWords("Easy", "horse", "лошадь"), 
+  new classWords("Easy", "house", "жилище"), 
+  new classWords("Easy", "idea", "идея"), 
+  new classWords("Easy", "king", "король"), 
+  new classWords("Easy", "land", "суша"), 
+  new classWords("Easy", "letter", "письмо"), 
+  new classWords("Easy", "life", "жизнь"), 
+  new classWords("Easy", "line", "линия"), 
+  new classWords("Easy", "list", "список"), 
+  new classWords("Easy", "love", "любовь"), 
+  new classWords("Easy", "men", "мужчины"), 
+  new classWords("Easy", "money", "Деньги"), 
+  new classWords("Easy", "month", "месяц"), 
+  new classWords("Easy", "mother", "мать"), 
+  new classWords("Easy", "mountain", "гора"), 
+  new classWords("Easy", "name", "имя"), 
+  new classWords("Easy", "night", "ночь"), 
+  new classWords("Easy", "north", "север"), 
+  new classWords("Easy", "number", "номер"), 
+  new classWords("Easy", "order", "заказ"), 
+  new classWords("Easy", "page", "страница"), 
+  new classWords("Easy", "paper", "бумага"), 
+  new classWords("Easy", "pen", "ручка"), 
+  new classWords("Easy", "people", "люди"), 
+  new classWords("Easy", "person", "человек"), 
+  new classWords("Easy", "phone", "Телефон"), 
+  new classWords("Easy", "picture", "Рисунок"), 
+  new classWords("Easy", "piece", "кусок"), 
+  new classWords("Easy", "place", "место"), 
+  new classWords("Easy", "plant", "растение"), 
+  new classWords("Easy", "problem", "Проблема"), 
+  new classWords("Easy", "product", "продукт"), 
+  new classWords("Easy", "question", "вопрос"), 
+  new classWords("Easy", "rive", "rречка"), 
+  new classWords("Easy", "rock", "Скала"), 
+  new classWords("Easy", "room", "комната"), 
+  new classWords("Easy", "school", "школа"), 
+  new classWords("Easy", "science", "наука"), 
+  new classWords("Easy", "sea", "море"), 
+  new classWords("Easy", "sentence", "предложение"), 
+  new classWords("Easy", "ship", "корабль"), 
+  new classWords("Easy", "side", "сторона"), 
+  new classWords("Easy", "sister", "сестра"), 
+  new classWords("Easy", "song", "песня"), 
+  new classWords("Easy", "sound", "звук"), 
+  new classWords("Easy","south", "юг"), 
+  new classWords("Easy", "space", "пространство"), 
+  new classWords("Easy", "state", "состояние"), 
+  new classWords("Easy", "story", "история"), 
+  new classWords("Easy", "thriller", "триллер"), 
+  new classWords("Easy", "top", "вершина"), 
+  new classWords("Easy", "tree", "дерево"), 
+  new classWords("Easy", "watch", "Часы"), 
+  new classWords("Easy", "water", "вода"), 
+  new classWords("Easy", "week", "неделя"), 
+  new classWords("Easy", "west", "запад"), 
+  new classWords("Easy", "wind", "ветер"), 
+  new classWords("Easy", "woman", "женщина"), 
+  new classWords("Easy", "wood", "лес"), 
+  new classWords("Easy", "word", "слово"), 
+  new classWords("Easy", "world", "мир"), 
+  new classWords("Easy", "year", "год") 
 ];
 const words2 = [
-  new classWords(2, "cherry", "вишня"),
-  new classWords(2, "orange", "апельсин") 
+  new classWords("Middle", "cherry", "вишня"),
+  new classWords("Easy","apple", "яблоко"),
+  new classWords("Middle", "orange", "апельсин") 
 ];
 const words3 = [
-  new classWords(3, "grape", "грейпфрукт"),
-  new classWords(3, "kiwi", "киви") 
+  new classWords("Easy","banana", "банан") ,
+  new classWords("Hard", "grape", "грейпфрукт"),
+  new classWords("Hard", "kiwi", "киви") 
 ];
 const startMenu = document.querySelector("#startMenu"); 
 const randomWords = document.querySelector("#output");
@@ -120,7 +221,7 @@ function render(a){
 function selectRandomWords(words){
     const randomIndex = Math.floor(Math.random() * words.length);
     const randomObject = words[randomIndex];
-    randomWords.innerHTML = `${randomObject.level} level select <p>${randomObject.word}, ${randomObject.translate}</p>`
+    randomWords.innerHTML = `<p class="level">${randomObject.level} level <br><br><p class="word">${randomObject.word}<br>(${randomObject.translate})</p></p>`
 }
 }
 
@@ -128,7 +229,7 @@ function selectRandomWords(words){
 function teamClick(team){
   const inputValue = parseInt(selectPoints.value)
   function counterInner(){
-    counter.innerHTML = team.name + " points: " + team.point;
+    counter.innerHTML = team.name + `<br> points: ` + team.point;
   }
   counterInner()
   yes.onclick = function(){
